0100c02afa30d071d33dd6f04140fbc40f6eb793
xxjcaxx <jocasal@gmail.com>
2024-11-13 15:39:29 +0100

    inici projecte

---
+++ projectecomplet/tests/tests.test.js
@@ -0,0 +1,10 @@
+import {describe, expect, test } from "vitest";
+
+describe("Javascript Básico", function () {
+    describe("Variables", function () {
+      test("Debe retornar un array elementos de los tipo solicitados", function () {
+        expect(1).toEqual(1)
+     
+      });
+    });
+});
